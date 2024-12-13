a68291be8d08ce9e446b28ec1978e44abba8fef7
LuquyPest <lylianfredon@gmail.com>
2024-12-13 16:07:30 +0100

    Comments

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
+#install SSH
 sudo systemctl enable ssh
 sudo systemctl start ssh
+#install FTP
 sudo apt install proftpd
-
+#install LIB for bluePY
 sudo apt-get install python-pip libglib2.0-dev
 sudo pip install bluepy
